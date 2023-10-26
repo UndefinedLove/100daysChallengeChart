8e2a7448cc1cab35ef9401e42691920750d95880
UndefinedLove <143685797+UndefinedLove@users.noreply.github.com>
2023-10-26 18:48:45 +0530

    some commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Function to set the status of a day in local storage
+function setDayStatus(day, status) {
+    localStorage.setItem(`day${day}`, status);
+}
+
+// Function to get the status of a day from local storage
+function getDayStatus(day) {
+    return localStorage.getItem(`day${day}`);
+}
+
+// Function to update the visual style of a day cell
+function updateDayCell(day) {
+    const cell = document.getElementById(`day${day}`);
+    const status = getDayStatus(day);
+
+    if (status === 'done') {
+        cell.classList.add('done');
+    }
+}
+
+// Event listener to handle cell clicks
+document.querySelectorAll('td').forEach((cell, index) => {
+    cell.addEventListener('click', () => {
+        const currentStatus = getDayStatus(index + 1);
+
+        if (!currentStatus || currentStatus === 'skipped') {
+            setDayStatus(index + 1, 'done');
+        } else {
+            setDayStatus(index + 1, 'skipped');
+        }
+
+        updateDayCell(index + 1);
+    });
+
+    updateDayCell(index + 1);
+});
+
+
+
+const addNumbers = () => {
+    for(let i=1; i<=100; i=i+1) {
+        document.getElementById(`day${i}`).innerHTML = i;
+    }
+}
+
+addNumbers();
+
+
+document.getElementById("resetButton").addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+});
